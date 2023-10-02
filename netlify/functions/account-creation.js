1b8387b6e46e90f0e5ffea3119a37ef795d9e256
Jesus Perez <jesusmpds18@gmail.com>
2023-10-02 17:20:08 -0300

    feat: create new account creation netlify function

---
+++ netlify/functions/account-creation.js
@@ -0,0 +1,132 @@
+const FoxySDK = require("@foxy.io/sdk");
+const { FOXY_REFRESH_TOKEN, FOXY_CLIENT_SECRET, FOXY_CLIENT_ID } = process.env;
+const foxy = new FoxySDK.Backend.API({
+  refreshToken: FOXY_REFRESH_TOKEN,
+  clientSecret: FOXY_CLIENT_SECRET,
+  clientId: FOXY_CLIENT_ID,
+});
+
+const customerByEmail = customerEmail =>
+  `https://api.foxycart.com/stores/107955/customers?email=${customerEmail}`;
+const createCustomer = "https://api.foxycart.com/stores/107955/customers";
+const allowedOrigins = [
+  "https://petscriptions-6d43af.webflow.io",
+  "https://petscriptions.ca",
+  "https://www.petscriptions.ca",
+];
+
+const errorResponse = details => {
+  return {
+    body: JSON.stringify({
+      ok: false,
+      details,
+    }),
+    statusCode: 500,
+  };
+};
+
+// Netlify Function
+exports.handler = async event => {
+  const { body, headers } = event;
+  const data = JSON.parse(body);
+
+  if (!allowedOrigins.includes(headers.origin)) {
+    return {
+      statusCode: 400,
+      body: {
+        ok: false,
+        details: " Not an allowed origin ",
+      },
+      headers: { "Access-Control-Allow-Origin": "" },
+    };
+  }
+
+  const customer = {
+    first_name: data.first_name,
+    last_name: data.last_name,
+    email: data.email,
+  };
+
+  const formCustomerAttributes = Object.keys(data).map(key => {
+    if (key.includes("Prescribing")) return { name: key, value: data[key], visibility: "public" };
+  });
+
+  const defaultAddress = {
+    address_name: "Me",
+    first_name: data.first_name,
+    last_name: data.last_name,
+    company: data.company,
+    address1: data.address1,
+    address2: data?.address2 ?? "",
+    city: data.city,
+    region: data.region,
+    postal_code: data.postal_code,
+    country: data.country,
+    phone: data.phone,
+    is_default_billing: true,
+    is_default_shipping: true,
+  };
+
+  // Check if customer exists
+  try {
+    const customerExists = await (await foxy.fetch(customerByEmail(customer.email))).json();
+
+    if (customerExists.returned_items) {
+      return {
+        body: JSON.stringify({
+          ok: true,
+          details: "Customer already exists",
+        }),
+        statusCode: 409,
+      };
+    }
+  } catch (error) {
+    console.log("ERROR: ", error);
+    return errorResponse("An Error has ocurred when fetching the customer by email");
+  }
+
+  try {
+    const res = await foxy.fetch(createCustomer, {
+      method: "POST",
+      body: JSON.stringify(customer),
+    });
+    const newCustomer = await res.json();
+    console.log("newCustomer", JSON.stringify(newCustomer));
+
+    const customerAttributes = newCustomer._links["fx:attributes"].href;
+    const customerDefaultShippingAddress = newCustomer._links["fx:default_shipping_address"].href;
+
+    const attributes = await (
+      await foxy.fetch(customerAttributes, {
+        method: "PATCH",
+        body: JSON.stringify(formCustomerAttributes),
+      })
+    ).json();
+
+    console.log("newCustomer attributes", attributes);
+
+    const address = await (
+      await foxy.fetch(customerDefaultShippingAddress, {
+        method: "POST",
+        body: JSON.stringify(defaultAddress),
+      })
+    ).json();
+
+    console.log("newCustomer address", address);
+
+    return {
+      body: JSON.stringify({
+        ok: true,
+        data: {
+          customer: newCustomer,
+          attributes,
+          address,
+        },
+      }),
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.log("ERROR: ", error);
+    return errorResponse("An Error has ocurred when creating the foxy customer");
+  }
+};
